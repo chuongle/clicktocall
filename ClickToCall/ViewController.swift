86b6ac702df1bef14e56e4df696dcdfa0e2444cf
Chuong Le <chuong@poeticsystems.com>
2017-06-30 14:42:58 -0500

    create simple swift app that take phone input and make a call.

--- ClickToCall/ViewController.swift
+++ ClickToCall/ViewController.swift
@@ -1,25 +1,48 @@
 //
 //  ViewController.swift
-//  ClickToCall
+//  clicktocall-swift
 //
 //  Created by Chuong Le on 6/30/17.
-//  Copyright © 2017 Chuong Le. All rights reserved.
+//  Copyright © 2017 Poetic Systems. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var callButton: UIButton!
+    @IBOutlet weak var phoneInput: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+        phoneInput.keyboardType = UIKeyboardType.numberPad
+        // for hitting return
+        phoneInput.delegate = self
+        // for tapping
+        self.view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(ViewController.dismissKeyboard)))
 
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func placeCall(_ sender: UIButton) {
+        UIApplication.shared.open(URL(string: "tel://" + phoneInput.text!)!, options: [:], completionHandler: nil)
+    }
+    
+    // for tapping
+    func dismissKeyboard() {
+        phoneInput.resignFirstResponder()
+    }
+    
+    // for hitting return
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        phoneInput.resignFirstResponder()
+        return true
+    }
+    
 }
 
